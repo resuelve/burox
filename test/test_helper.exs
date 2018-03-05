0ceae498de781cb983dcf545fe77294d9480587b
Erick Madrid <stereopony@outlook.com>
2018-03-05 10:09:53 -0600

    Se agrega biblioteca de mox

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,5 @@
+ExUnit.start()
+
 # Mock de respuesta del buro
 Mox.defmock(Burox.BuroService.Mock, for: Burox.BuroService)
 Application.put_env(:burox, :buro_service, Burox.BuroService.Mock)
-
-ExUnit.start()
