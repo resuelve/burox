a73f37aa08e173fd9bc1d5c68c2e1743d5f4e577
Octavio Juárez Molina <bluesrapper@gmail.com>
2022-08-18 17:42:48 -0500

    add env vars to config files

--- config/test.exs
+++ config/test.exs
@@ -5,6 +5,12 @@ config :logger, level: :warn
 config :burox,
   buro_user: "user",
   buro_password: "password",
+  buro_user_sae: "user_sae",
+  buro_password_sae: "password_sae",
+  buro_user_special: "user_special",
+  buro_password_special: "password_special",
+  buro_user_special_sae: "user_special_sae",
+  buro_password_special_sae: "password_special_sae",
   buro_user_propsector: "user",
   buro_password_prospector: "password",
   buro_service: Burox.BuroService.Mock
