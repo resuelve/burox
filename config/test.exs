42bbed70e01caeb8b7eb6eb89529b5a32a0b51b4
Octavio Juárez Molina <bluesrapper@gmail.com>
2022-08-22 08:57:31 -0500

    add unit tests for encoder module

--- config/test.exs
+++ config/test.exs
@@ -11,6 +11,6 @@ config :burox,
   buro_password_special: "password_special",
   buro_user_special_sae: "user_special_sae",
   buro_password_special_sae: "password_special_sae",
-  buro_user_propsector: "user",
-  buro_password_prospector: "password",
+  buro_user_prospector: "user_prospector",
+  buro_password_prospector: "password_prospector",
   buro_service: Burox.BuroService.Mock
